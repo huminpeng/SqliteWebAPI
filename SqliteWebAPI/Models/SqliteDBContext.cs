a9c4f0ddfb7987d367ffc82ee1c5bda86f637907
HU Minpeng <min-peng.hu@leica-geosystems.com.cn>
2020-07-01 13:54:02 +0800

    添加项目文件。

---
+++ SqliteWebAPI/Models/SqliteDBContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Sqlite;
+
+namespace SqliteWebAPI.Models
+{
+    public class SqliteDBContext : DbContext
+    {
+        public SqliteDBContext(DbContextOptions<SqliteDBContext> options) : base(options)
+        {
+
+        }
+        public DbSet<User> Users { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().ToTable("User");
+        }
+    }
+}
---
+++ SqliteWebAPI/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SqliteWebAPI.Models
+{
+    public class User
+    {
+        public int ID
+        {
+            get; set;
+        }
+        public String Name
+        {
+            get; set;
+        }
+    }
+}
